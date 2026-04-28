96e628d76e43328e063897496b4050621fef8614
F100Pilot <pflm.bet@gmail.com>
2026-04-28 16:04:31 +0100

    fix: limit subtitles to en and pt only

--- subtitles.js
+++ subtitles.js
@@ -50,7 +50,7 @@ async function searchSubtitles(imdbId, season, episode) {
 
   const params = {
     imdb_id:         imdbId,
-    languages:       'en,pt-BR,pt',
+    languages:       'en,pt',
     order_by:        'download_count',
     order_direction: 'desc',
   };
